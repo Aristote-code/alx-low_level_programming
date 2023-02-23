0b81feea06a0eecb439914ed8c07d3ef7178b41d
root <a.gahima@gmail.com>
2023-02-23 12:44:39 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_numbers(void)
+{
+int a;
+for(a = 0; a <= 9; a++)
+_putchar(a + '0')
+_putchar('\n')
+return;
+}
