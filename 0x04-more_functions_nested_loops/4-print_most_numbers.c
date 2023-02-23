97e4cb5f8251a84f9df67b1eba01e4fdd66e8e98
root <a.gahima@gmail.com>
2023-02-23 14:02:08 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints
+* owned by Bwave ICT
+*Return: void
+*/
+
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+if (i == 2 || i == 4)
+{
+continue
+}
+else
+{
+_putchar(i);
+
+_putchar('\n')
+}
+}
+}
