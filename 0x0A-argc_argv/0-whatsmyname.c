e3147a75f3beff939debc0edad2cf50db4737129
root <a.gahima@gmail.com>
2023-03-11 03:46:26 -0800

    yes

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints its name, followed by a new line.
+* @argc: argument count
+* @argv: arguments
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+_putchar("%s\n", argv[0]);
+return (0);
+}
+
