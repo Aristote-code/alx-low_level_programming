8a9bea657afb4ca585e75b2f8c6c6b64a43760a1
Aristote-code <gahimaaristote1@gmail.com>
2023-05-22 22:21:08 +0200

    update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,29 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
-/**
- * main - Prints the alphabet in lowercase.
- *
- * Return: Always 0.
- */
+/* betty style doc for function main goes there */
 int main(void)
 {
-char letter;
+	int n;
+    int x;
 
-for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+    x = n % 10;
 
-putchar('\n');
-
-return (0);
+    if (x > 5)
+    {
+        printf("Last digit of %d is %d and is greater than 5\n", n, x);
+    }
+    else if (x == 0)
+    {
+        printf("Last digit of %d is %d and is 0\n", n, x);
+    }
+    else if (n > 6 & n != 0)
+    {
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+    }
+	return (0);
 }
