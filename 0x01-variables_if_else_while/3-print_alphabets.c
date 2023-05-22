5f0c8b085f422527b6352cd3e3ad28e80d26a088
Aristote-code <gahimaaristote1@gmail.com>
2023-05-22 22:39:02 +0200

    update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, and in uppercase
+ * main - Prints the alphabet in lowercase.
  *
- * Return: 0
+ * Return: Always 0.
  */
+
 int main(void)
 {
-char letter;
+    char letter;
+    for (letter = 'a'; letter <= 'z' ; letter++)
+    {
+        putchar(letter);
+    }
 
-for (letter = 'a'; letter <= 'z'; letter++)
-putchar(letter);
-
-for (letter = 'A'; letter <= 'Z'; letter++)
-putchar(letter);
-putchar('\n');
-return (0);
+    putchar('\n');
 }
