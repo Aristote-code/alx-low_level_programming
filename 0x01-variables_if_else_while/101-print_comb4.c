4affd27f629ad48c6f898d485f983649448554e8
Aristote-code <gahimaaristote1@gmail.com>
2023-05-23 01:54:42 +0200

    update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,30 +7,27 @@
  */
 int main(void)
 {
-int number1,number2,number3;
+    int number1, number2, number3;
 
-for (number1 = 0; number1 < 8; number1++)
-{
-for (number2 = 0; number2 < 9; number2++)
-{
-for (number3 = 0; number3 < 10; number3++)
-{
-if (number1 < number2 || number2 < number3)
-{
-putchar((number1 % 10) + '0');
-putchar((number2 % 10) + '0');
-putchar((number3 % 10) + '0');
-if (number1 == 7 && number2 == 8 && number3 == 9)
-{
-continue;
-}
+    for (number1 = 0; number1 <= 7; number1++)
+    {
+        for (number2 = number1 + 1; number2 <= 8; number2++)
+        {
+            for (number3 = number2 + 1; number3 <= 9; number3++)
+            {
+                putchar((number1 % 10) + '0');
+                putchar((number2 % 10) + '0');
+                putchar((number3 % 10) + '0');
 
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-}
-return (0);
+                if (number1 == 7 && number2 == 8 && number3 == 9)
+                    continue;
+
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+    return (0);
 }
