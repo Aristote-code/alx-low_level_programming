20fc9703fa341025d2a895fca9965d87e0363ab1
Aristote-code <gahimaaristote1@gmail.com>
2023-05-22 22:57:28 +0200

    update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, except for q and e.
+ * main - Prints the alphabet in lowercase, except q and e.
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 char letter;
-
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-if (letter != 'e' && letter != 'q')
+if (letter != 'e' || letter != 'q')
+{
 putchar(letter);
+}
 }
 
 putchar('\n');
 
-return (0);
+return(0);
+
 }
