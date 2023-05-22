f766694fc40cc213a6de1510b3279f90e23e9224
Aristote-code <gahimaaristote1@gmail.com>
2023-05-23 00:56:05 +0200

    update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,7 +12,12 @@ int main(void)
 
     for (number = 0; number < 10; number++)
     {
+        
         putchar((number % 10) + '0');
+        if (number == 9)
+        {
+            continue;
+        }
         putchar(',');
         putchar(' ');
     }
