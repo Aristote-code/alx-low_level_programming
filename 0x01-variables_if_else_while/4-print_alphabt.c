ee212ead65fbe6b6d39732b8ecc4047031125c16
Aristote-code <gahimaaristote1@gmail.com>
2023-05-22 23:01:29 +0200

    update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-if (letter != 'e' || letter != 'q')
+if (letter != 'e' & letter != 'q')
 {
 putchar(letter);
 }
@@ -19,6 +19,6 @@ putchar(letter);
 
 putchar('\n');
 
-return(0);
+return (0);
 
 }
