feb3e06bbcaf25d90fce7ea5a77f2cc240b73523
Aristote-code <gahimaaristote1@gmail.com>
2023-05-29 16:31:10 +0200

    updating File: 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+/*
+ * File: 3-puts.c
+ * Auth: Gahima Aristote
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+
+void _puts(char *str)
+{
+while(*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
