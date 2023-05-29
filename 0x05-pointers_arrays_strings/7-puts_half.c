c2c8ecbec09331e38b76364144aa5cf1f0304460
Aristote-code <gahimaaristote1@gmail.com>
2023-05-29 22:49:07 +0200

    updating 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,29 +11,29 @@
  */
 void puts_half(char *str)
 {
-    int index = 0;
-    int len = 0;
-    int n;
+int index = 0;
+int len = 0;
+int n;
 
-    while (str[index] != '\0')
-    {
-        len++;
-        index++;
-    }
+while (str[index] != '\0')
+{
+len++;
+index++;
+}
 
-    if ((len % 2) == 0)
-    {
-        n = len / 2;
-    }
-    else
-    {
-        n = (len + 1) / 2;
-    }
+if ((len % 2) == 0)
+{
+n = len / 2;
+}
+else
+{
+n = (len + 1) / 2;
+}
 
-    for (index = n; index < len; index++)
-    {
-        _putchar(str[index]);
-    }
+for (index = n; index < len; index++)
+{
+_putchar(str[index]);
+}
 
-    _putchar('\n');
+_putchar('\n');
 }
