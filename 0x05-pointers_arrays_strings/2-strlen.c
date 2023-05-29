c69da0b4271da404f5dd7e19d19c9e6a5b8b1acd
Aristote-code <gahimaaristote1@gmail.com>
2023-05-29 16:07:15 +0200

    updating File: 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,20 +7,19 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
+ * @s: The string to get the length of.
  *
- * Return: The length of @str.
+ * Return: The length of the string.
  */
-
 int _strlen(char *s)
 {
-int count = 0;
-
-while(*s != '\0')
-{
-count++;
-s++;
-}
-
-return (count);
+    int count = 0;
+    
+    while (*s != '\0')
+    {
+        count++;
+        s++;
+    }
+    
+    return count;
 }
