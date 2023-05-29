aa63d3243da56efa978d7ca214cb30d467536b84
Aristote-code <gahimaaristote1@gmail.com>
2023-05-29 15:44:52 +0200

    updating File: 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Gahima Aristote
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+
+int _strlen(char *s)
+{
+int count = 0,i;
+while(*s[i] != '\0')
+{
+count++;
+i++;
+}
+}
