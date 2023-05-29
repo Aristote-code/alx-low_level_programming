a1fa7290063e975eada63d4b393e9d5ec57220cf
Aristote-code <gahimaaristote1@gmail.com>
2023-05-29 18:27:52 +0200

    updating File: 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+/*
+ * File: 5-rev_string.c
+ * Auth: Gahima Aristote
+ */
+
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+    int i;
+    char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= (length / 2); i--)
+	{
+		temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+
+}
