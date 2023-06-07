6c66c91e4d95c7ec0e39ceca032e2c9f15b56315
Aristote-code <gahimaaristote1@gmail.com>
2023-06-07 19:04:03 +0200

    updating File: 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Auth: Gahima Aristote
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+int length = 0;
+if (*s != '\0')
+{
+length++;
+length = 1 + _strlen_recursion(s + 1);
+}
+return(length);
+}
