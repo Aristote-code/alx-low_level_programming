72d2180ad63ed6ccbbd7b1e94a5181017207bd4e
Aristote-code <gahimaaristote1@gmail.com>
2023-06-07 18:26:39 +0200

    updating multiple files

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,14 +10,15 @@
  * @s: The string to be printed.
  */
 
-void _puts_recursion(char *s){
-if(*s == '\0')
+void _puts_recursion (char *s)
+{
+if (*s == '\0')
 {
 _putchar('\n');
 }
 else
 {
 _putchar(*s);
-_puts_recursion(s+1);
+_puts_recursion(s + 1);
 }
 }
