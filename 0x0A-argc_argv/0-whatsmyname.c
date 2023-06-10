cca1c99953a3ed222ea4c3cc9bb659537aac951c
Aristote-code <gahimaaristote1@gmail.com>
2023-06-10 14:51:15 +0200

    updating multiple files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,6 +4,7 @@
  */
 
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Prints the program name, followed by a new line.
@@ -14,6 +15,6 @@
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-_putchar(argv[0]);
+printf("%s\n",argv[0]);
 return (0);
 }
