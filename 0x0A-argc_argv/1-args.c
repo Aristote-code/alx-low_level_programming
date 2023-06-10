31451380121646ca3ea80d68cd1c68eddf4d9a42
Aristote-code <gahimaaristote1@gmail.com>
2023-06-10 15:21:05 +0200

    Updating file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -14,9 +14,10 @@
  * Return: Always 0.
  */
 
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
-printf("%d", argc - 1);
-return (0);
+	printf("%d\n", argc - 1);
+
+	return (0);
 }
