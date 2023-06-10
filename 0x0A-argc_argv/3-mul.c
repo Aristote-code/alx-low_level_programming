89c6cb464f7c7a74083e6aca5b530bdb92c7eb73
Aristote-code <gahimaaristote1@gmail.com>
2023-06-10 18:55:01 +0200

    updating

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,34 @@
+/*
+ * File: 3-mul.c
+ * Auth: Gahima Aristote
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Multiplies two integer command-line arguments.
+ * main - Prints the product of two integer arguments.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: Always 0.
  */
+
 int main(int argc, char *argv[])
 {
-    int num1, num2, mul;
+int num1, num2, mul;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 1;
-    }
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-    mul = num1 * num2;
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+mul = num1 * num2;
 
-    printf("%d\n", mul);
+printf("%d\n", mul);
 
-    return 0;
+return(0);
 }
