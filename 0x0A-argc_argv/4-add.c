367f13c2af6dbb761cb3d37c55e977b37e71f879
Aristote-code <gahimaaristote1@gmail.com>
2023-06-10 20:23:45 +0200

    Updating file 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,38 @@
-/*
- * File: 4-add.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main - Prints the addition of positive numbers,
- *        followed by a new line.
- * @argc: The number of arguments passed to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: If one of the numbers contains symbols that are non-digits - 1.
- *         Otherwise - 0.
- */
-
-int main(int argc, char *argv[])
+* main - adds positive numbers.
+* @argc: argument count
+* @argv: arguments
+*
+* Return: 0
+*/
+int main(int argc, char **argv)
 {
-int i, j, sum = 0;
-if (argc == 1)
+int i, n, sum = 0;
+char *flag;
+
+if (argc < 2)
 {
 printf("0\n");
+return (0);
 }
-for (i = 1; i < argc; i++)
+
+for (i = 1; argv[i]; i++)
 {
-for (j = 0; argv[i][j] != '\0'; j++)
-{
-if (argv[i][j] < '0' || argv[i][j] > '9')
+n = strtol(argv[i], &flag, 10);
+if (*flag)
 {
 printf("Error\n");
 return (1);
 }
+else
+{
+sum += n;
 }
-sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
+
 return (0);
 }
