d331efe833a3b9603758e3303d7f6ad09e987db8
Aristote-code <gahimaaristote1@gmail.com>
2023-06-10 14:43:13 +0200

    Updating main.h file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,8 +12,8 @@
  *
  * Return: Always o.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-printf("%s",argv[0]);
+printf("%s\n",argv[0]);
 return (0);
 }
