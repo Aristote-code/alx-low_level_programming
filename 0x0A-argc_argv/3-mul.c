d8dc4b3681ec418e6a1e37673cc792e5ffac3f6b
Aristote-code <gahimaaristote1@gmail.com>
2023-06-10 18:21:28 +0200

    Updating files

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,33 @@
+/*
+ * File: 3-mul.c
+ * Auth: Gahima Aristote
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
-* main - multiplies two numbers.
-* @argc: argument count
-* @argv: arguments
-*
-* Return: 0
-*/
-int main(int argc, char **argv)
-{
-int x, y;
+ * main - Prints the number of arguments passed to it.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
 
-if (argc < 3)
+int main(int argc, char *argv[])
+{
+int num1, num2, mul;
+
+if (argc != 3)
 {
 printf("Error\n");
-return (1);
 }
 
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-printf("%d\n", x * y);
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+mul = num1 * num2;
 
-return (0);
+printf("%d", mul);
+
+return(0);
 }
