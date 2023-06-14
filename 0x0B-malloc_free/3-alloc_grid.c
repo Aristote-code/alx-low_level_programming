3e8b79c35daf764957c534ab30ac1a67ac3d38c4
Aristote-code <gahimaaristote1@gmail.com>
2023-06-14 23:12:08 +0200

    Updating ast file

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,53 @@
+/*
+ * File: 3-alloc_grid.c
+ * Auth: Gahima Aristote
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ *               integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the 2-dimensional array of integers.
+ */
+int **alloc_grid(int width, int height)
+{
+int j,i;
+int **grid;
+if (width <= 0)
+{
+return (NULL);
+}
+if (height <= 0)
+{
+return (NULL);
+}
+grid = malloc(sizeof(int *) * height);
+if (grid == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(sizeof(int) * width);
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+{
+free(grid[j]);
+}
+free(grid);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+{
+grid[i][j] = 0;
+}
+}
+return(grid);
+}
