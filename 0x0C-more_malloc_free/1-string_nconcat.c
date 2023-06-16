ac3571d876ef84f14134b9d35498ce6bcdde6ec8
Aristote-code <gahimaaristote1@gmail.com>
2023-06-16 18:02:46 +0200

    Updating new file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,37 +18,29 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int index, len = 0, len2 = 0;
-char *concat;
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
-while (s1[len] != '\0')
-{
-len++;
-}
-while (s2[len2] != '\0' && len2 < n)
-{
-len2++;
-}
-concat = malloc(sizeof(char) * (len + len2 + 1));
-if (concat == NULL)
-{
-return (NULL);
-}
-for (index = 0; index < len; index++)
-{
-concat[index] = s1[index];
-}
-for (index = 0; index < len2; index++)
-{
-concat[index + len] = s2[index];
-}
-concat[len + len2] = '\0';
-return (concat);
+    int index, len = 0, len2 = 0;
+    char *concat;
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    while (s1[len] != '\0')
+        len++;
+
+    while (s2[len2] != '\0' && len2 < n)
+        len2++;
+
+    concat = malloc(sizeof(char) * (len + len2 + 1));
+    if (concat == NULL)
+        return (NULL);
+
+    for (index = 0; index < len; index++)
+        concat[index] = s1[index];
+
+    for (index = 0; index < len2; index++)
+        concat[len + index] = s2[index];
+
+    concat[len + len2] = '\0';
+    return (concat);
 }
