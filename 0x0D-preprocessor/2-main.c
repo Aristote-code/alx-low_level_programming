03d784815d75f67e49e84d3789f9c8b46b1abfa8
Aristote-code <gahimaaristote1@gmail.com>
2023-06-19 14:18:32 +0200

    Updating File: 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-main.c
+ * Auth: Gahima Aristote
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+printf("%s\n", __FILE__);
+
+return (0);
+}
