e6ca73f5a25b1d8489aae4c2fb9ae513fe6256ef
Aristote-code <gahimaaristote1@gmail.com>
2023-06-19 13:57:54 +0200

    uPDATING File: 0-object_like_macro.h

--- 0x0D. C - Preprocessor/0-object_like_macro.h
+++ 0x0D. C - Preprocessor/0-object_like_macro.h
@@ -1,1 +1,13 @@
+#ifndef OBJECT_LIKE _MACRO_H
+#define OBJECT_LIKE _MACRO_H
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: gAHIMA arISTOTE
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
 #define SIZE 1024
+
+#endif
